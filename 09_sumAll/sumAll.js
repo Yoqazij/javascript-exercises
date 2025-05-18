72b21e1bb45a78140e0f0cf134db339af991f02b
Yoqazij <n-hug@ik.me>
2025-05-18 07:01:45 +0200

    Solve lesson 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+let sum = 0;
+let smallest = Math.min(a, b);
+let largest = Math.max(a, b);
 
+if(smallest < 0) return "ERROR";
+if(!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+
+
+for (let i = smallest; i <= largest; i++){
+    sum += i;
+}
+return sum;
 };
 
 // Do not edit below this line
