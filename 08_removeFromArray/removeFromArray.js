d630cf24be4ffa7be6bf436ab392919e6c423686
Yoqazij <n-hug@ik.me>
2025-05-18 06:37:12 +0200

    Finish lesson 8.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
+
+    const newArray = array.filter(v => !valuesToRemove.includes(v))
+    return (newArray);
 };
 
 // Do not edit below this line
