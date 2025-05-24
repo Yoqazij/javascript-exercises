393cf7ea0ec7e060b6a7724e3cd037966469c217
Yoqazij <n-hug@ik.me>
2025-05-24 11:33:04 +0200

    Finish lesson 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+        const currentYear = new Date().getFullYear();
+        const age = people.map(person => (person.yearOfDeath || currentYear) - person.yearOfBirth);
+        const highestAge = Math.max(...age);
+        const highestAgeIndex = age.indexOf(highestAge);
 
+return people [highestAgeIndex]; 
 };
 
 // Do not edit below this line
